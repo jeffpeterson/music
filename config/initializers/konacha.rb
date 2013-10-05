f592587c48c822a7825ce974d7ae541656cae121
Jeff Peterson <jeff@petersonj.com>
2013-10-05 16:49:36 -0500

    ui fanciness. blurred #bar

--- config/initializers/konacha.rb
+++ config/initializers/konacha.rb
@@ -1,6 +1,6 @@
 Konacha.configure do |config|
   require 'capybara/poltergeist'
-  config.spec_dir     = 'app/assets/javascripts'
-  config.spec_matcher = 'spec.js.coffee'
+  # config.spec_dir     = 'app/assets/javascripts'
+  # config.spec_matcher = 'spec.js.coffee'
   config.driver       = :poltergeist
 end if defined?(Konacha)
