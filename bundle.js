e77897e13897f82b0cf08a2abeac2e274f8c8108
Jeff Peterson <jeff@petersonj.com>
2014-12-18 23:17:56 -0800

    lower volume

--- bundle.js
+++ bundle.js
@@ -282,9 +282,13 @@ var ctx = new AudioContext()
 var el = document.createElement('audio')
 var source = ctx.createMediaElementSource(el)
 var analyser = ctx.createAnalyser()
+var gain = ctx.createGain()
 
 source.connect(analyser)
-analyser.connect(ctx.destination)
+analyser.connect(gain)
+gain.connect(ctx.destination)
+
+gain.gain.value = 0.1
 
 module.exports = {
   play: function play(url) {
