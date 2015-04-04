be364415e275c050e60f28bf7fe8579e0af03f15
Jeff Peterson <jeff@petersonj.com>
2015-04-04 01:38:06 -0700

    simplify waveform

--- components/WaveForm.js
+++ components/WaveForm.js
@@ -18,54 +18,40 @@ export class WaveForm extends Base {
     analyser.smoothingTimeConstant = 0.5
 
     var buffer = new Uint8Array(analyser.fftSize)
-    var freqBuffer = new Float32Array(analyser.frequencyBinCount)
     var that = this
 
     ctx.lineWidth = 4
-    ctx.fillStyle = 'rgba(0,0,0, 0)'
+
+    requestAnimationFrame(draw)
 
     function draw() {
       requestAnimationFrame(draw)
       analyser.getByteTimeDomainData(buffer)
-      analyser.getFloatFrequencyData(freqBuffer)
-      var bassLevel = Math.max(0, calcBassLevel(freqBuffer))
-      let alpha = (bassLevel * 2)
       let {colors} = that.props
-      var x, y
 
-      ctx.strokeStyle = rgb(mix(colors[0], colors[1], alpha))
-
-      ctx.globalCompositeOperation = 'destination-in'
-      ctx.fillRect(0, 0, width, height)
+      ctx.strokeStyle = rgb(colors[0])
+      ctx.clearRect(0, 0, width, height)
 
       ctx.beginPath()
 
-      for (var i = 0, l = buffer.length; i < l; i++) {
-        x = i * width / l
-        // y = -buffer[i]
-        y = 0.00390625 * buffer[i] * height
-
-        if(i === 0) {
-          ctx.moveTo(x, y);
-        } else {
-          ctx.lineTo(x, y);
-        }
+      for (let i = 0, l = buffer.length; i < l; i++) {
+        ctx.lineTo(i * width / l, 0.00390625 * buffer[i] * height)
       }
 
-      ctx.globalCompositeOperation = 'source-over'
-      ctx.globalAlpha = that.opacity()
       stroke(ctx)
     }
-
-    requestAnimationFrame(draw)
   }
 
   render() {
-    return <canvas className="WaveForm" />
-  }
+    let {
+      props: {isDimmed}
+    } = this
 
-  opacity() {
-    return this.props.isDimmed ? 0.2 : 1
+    let style = {
+      opacity: isDimmed ? 0.2 : 1
+    }
+
+    return <canvas style={style} className="WaveForm" />
   }
 }
 
@@ -94,12 +80,6 @@ function incAvg(avg, x, i) {
 
 function avg(nums) {
   return nums.reduce(incAvg)
-}
-
-var pBassLevel = 0
-
-function calcBassLevel(f) {
-  return 1 - avg([f[0], f[1], f[2], f[3]]) / avg([f[50], f[100], f[300], f[600]])
 }
 
 function stroke(ctx) {
